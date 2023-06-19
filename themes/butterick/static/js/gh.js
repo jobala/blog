e50fa5961b538b641b923bacfe7b5b290a18adea
jobala <japhethobalak@gmail.com>
2023-06-19 13:19:59 +0100

    chore: style embedded snippets

--- themes/butterick/static/js/gh.js
+++ themes/butterick/static/js/gh.js
@@ -1,22 +1,47 @@
 shortcodes = document.getElementsByClassName('gh')
 
+const Language = {
+  go: 'go',
+  toml: 'toml',
+  js: 'javascript',
+  json: 'json',
+  yml: 'yaml',
+  yaml: 'yaml',
+}
+
 for (let i = 0; i < shortcodes.length; i++) {
   const url = new URL(shortcodes[i].innerHTML)
+  shortcodes[i].innerHTML = ''
   const [start, stop] = getLineNumbers(url.hash)
   const code = document.createElement('code')
-  code.setAttribute('class', 'language-go')
+  const language = getLanguage(url.pathname)
+  const snippetPath = getSnippetPath(url.pathname)
+  code.setAttribute('class', 'snippet')
+
+  // add snippet header
+  const snippetHeader = document.createElement('div')
+  const linkToSnippet = document.createElement('a')
+  linkToSnippet.setAttribute('href', url.href)
+  linkToSnippet.setAttribute('target', '_blank')
+  linkToSnippet.innerHTML = snippetPath
+  snippetHeader.setAttribute('class', 'snippet-header')
+  snippetHeader.appendChild(linkToSnippet)
+  shortcodes[i].appendChild(snippetHeader)
+
+  const snippetContainer = document.createElement('div')
+  snippetContainer.setAttribute('class', 'snippet-container')
+  snippetContainer.appendChild(code)
+  shortcodes[i].appendChild(snippetContainer)
 
   fetch('https://raw.githubusercontent.com/' + url.pathname.replace('/blob', '')).then(async (res) => {
     r = await res.text()
     lines = r.split('\n')
 
-    for (let j = start; j < stop + 1; j++) {
+    for (let j = start - 1; j < stop + 1; j++) {
       const div = document.createElement('div')
-      div.innerHTML = hljs.highlight(lines[j], { language: 'golang', style: 'github' }).value
+      div.innerHTML = hljs.highlight(lines[j], { language, style: 'github' }).value
       code.appendChild(div)
     }
-
-    shortcodes[i].appendChild(code)
   })
 }
 
@@ -29,4 +54,23 @@ function getLineNumbers(hash) {
   start = res[0].slice(1)
   stop = res[1].slice(1)
   return [Number(start), Number(stop)]
+}
+
+/**
+ * getLanguage gets the programming language from a pathname
+ * @param {string} pathname
+ */
+function getLanguage(pathname) {
+  const parsed = pathname.split('/')
+  const file = parsed[parsed.length - 1]
+  const extension = file.split('.')[1]
+  return Language[extension]
+}
+
+/**
+ * getSnippetPath gets the path to a snippet file from pathname
+ */
+function getSnippetPath(pathname) {
+  const parsed = pathname.split('/')
+  return parsed[2] + '/' + parsed.slice(5).join('/')
 }
